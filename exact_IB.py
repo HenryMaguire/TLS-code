db625bbe7056ade38e226fd66a10be96e498db9f
Henry <henrymaguire>
2017-05-23 15:52:56 +0100

    An exact solution to TLS dynamics has been coded up in Python. It agrees within at least one parameter regime (overdamped) with the mathematica code. Now to calculate absorption.@

--- exact_IB.py
+++ exact_IB.py
@@ -1,45 +1,68 @@
-import scipy as sp
+from scipy.integrate import quad
 from qutip import ket, qeye, tensor, destroy, Qobj
 import numpy as np
 import matplotlib.pyplot as plt
-import sympy.functions.elementary.hyperbolic as hyp
+from sympy.functions import coth
+from utils import *
 
-def integrand(omega, t, alpha, KT, wc):
-    return -4.*alpha*(omega*wc/(omega**2 + wc**2))*hyp.coth(omega/2*KT)*(1-np.cos(omega*t))
+def get_data():
 
-def exact_decay(t, alpha, KT, wc):
-    I = np.array(sp.integrate.quad(integrand, 0, np.infty, args=(t, alpha, KT, wc)))
+    rho = [rho_01, rho_10]
+    return
+
+
+def integrand(omega, t, alpha, beta, Gamma, omega_0):
+    if omega ==0:
+        return 0.
+    else:
+        return (1/omega**2)*J_underdamped(omega, alpha, Gamma, omega_0)*coth(beta*omega/2)*(1-np.cos(omega*t))
+
+def plot_integrand():
+    omega = np.linspace(0.0001,20000,100000)
+    y = []
+    for i in omega:
+        y.append(integrand(i, 1, alpha, beta, Gamma, omega_0))
+    plt.plot(omega, y)
+    plt.show()
+
+def exact_decay(t, alpha, beta, Gamma, omega_0):
+    I = -np.array(quad(integrand, 0.00001, 20000, args=(t, alpha, beta, Gamma, omega_0),limit=150))
     return I
 
-def exact_solution(t, eps, wc, KT, rho_init):
-    Gamma = exact_decay(t, alpha, KT, wc)[0]
-    print Gamma
-    rho_00 = rho_init.matrix_element(G.dag(), G)
-    rho_11 = rho_init.matrix_element(E.dag(), E)
+def exact_solution_at_t(t, alpha, beta, Gamma, omega_0, rho_init):
+    Gamma = exact_decay(t, alpha, beta, Gamma, omega_0)[0]
     rho_01 = rho_init.matrix_element(G.dag(), E)*np.exp(Gamma)*np.exp(-1j*eps*t)
-    rho_10 = rho_init.matrix_element(E.dag(), G)*np.exp(Gamma)*np.exp(1j*eps*t)
-    rho = Qobj([[rho_00, rho_01],[rho_10, rho_11]])
-    return rho
+    return rho_01
 
 
-print integrand(1., 2., 0.1, 1., 10.)
+def exact_solution(eps, wc, beta, rho_init, a, b, points):
+    T = np.linspace(a, b, points)
+    rho_t = []
+    for t in T:
+        rho_t.append(exact_solution_at_t(t, alpha, beta, Gamma, omega_0, rho_init))
+    return T, rho_t
 
-G = ket([0])
-E = ket([1])
 
-x = np.arange(1.,5,0.5)
-alpha = 1.
-KT = 300.
-wc = 53.
-eps = 10.
-t = 0.001
+if __name__ == "__main__":
 
-rho_1 = G*G.dag()
-rho_2 = E*E.dag()
-rho_3 = 0.5*(G+E)*(E.dag()+G.dag())
-
-print exact_decay(0.1, alpha, KT, wc)
-print exact_solution(t, eps, wc, KT, rho_1)
-print exact_solution(t, eps, wc, KT, rho_2)
-print exact_solution(t, eps, wc, KT, rho_3)
-#plt.plot(x, integrand(x, 0, alpha, KT, wc))
+    G = ket([0])
+    E = ket([1])
+    rho_init = 0.5*(G+E)*(E.dag()+G.dag())
+    #x = np.arange(1.,5,0.5)
+    alpha = 400/np.pi
+    beta = 1/(0.695*300)
+    eps = 8000.
+    wc = 153.
+    omega_0 = 2000.
+    Gamma = omega_0**2/wc
+    T, rho_t = exact_solution(eps, wc, beta, rho_init, 0, 0.02, 1000)
+    reData = np.loadtxt("DATA/RealExactdata.dat")
+    imData = np.loadtxt("DATA/ImagExactdata.dat")
+    plt.plot(list(zip(*reData)[0]), list(zip(*reData)[1]), label='math real')
+    #plt.plot(list(zip(*imData)[0]), list(zip(*imData)[1]), 'math imag')
+    plt.plot(T, np.array(rho_t).real, label='real')
+    #plt.plot(T, np.array(rho_t).imag, label='imag')
+    plt.legend()
+    plt.show()
+    #t = 1
+    print exact_decay(1., alpha, beta, Gamma, omega_0)
---
+++ utils.py
@@ -0,0 +1,67 @@
+import numpy as np
+from numpy import pi
+import scipy as sp
+from qutip import spre, spost, sprepost
+import qutip as qt
+import pickle
+
+
+
+def load_obj(name ):
+    with open(name + '.pickle', 'rb') as f:
+        return pickle.load(f)
+
+def save_obj(obj, name ):
+    with open(name + '.pickle', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def Occupation(omega, T, time_units='cm'):
+    conversion = 0.695
+    if time_units == 'ev':
+        conversion == 8.617E-5
+    if time_units == 'ps':
+        conversion == 0.131
+    else:
+        pass
+    n =0.
+    beta = 0.
+    if T ==0.: # First calculate beta
+        n = 0.
+        beta = np.infty
+    else:
+        # no occupation yet, make sure it converges
+        beta = 1. / (conversion*T)
+        if sp.exp(omega*beta)-1 ==0.:
+            n = 0.
+        else:
+            n = float(1./(sp.exp(omega*beta)-1))
+    return n
+
+
+def J_multipolar(omega, Gamma, omega_0):
+    return Gamma*(omega**3)/(2*np.pi*(omega_0**3))
+
+def J_minimal(omega, Gamma, omega_0):
+    return Gamma*omega/(2*np.pi*omega_0)
+
+def J_flat(omega, Gamma, omega_0):
+    return Gamma
+
+def J_overdamped(omega, alpha, wc):
+    return alpha*wc*omega/(omega**2+wc**2)
+
+def J_underdamped(omega, alpha, Gamma, omega_0):
+    return alpha*Gamma*pow(omega_0,2)*omega/(pow(pow(omega_0,2)-pow(omega,2),2)+(Gamma**2 *omega**2))
+
+def rate_up(w, n, gamma, J, w_0):
+    rate = 0.5 * pi * n * J(w, gamma, w_0)
+    return rate
+
+def rate_down(w, n, gamma, J, w_0):
+    rate = 0.5 * pi * (n + 1. ) * J(w, gamma, w_0)
+    return rate
+
+def lin_construct(O):
+    Od = O.dag()
+    L = 2. * spre(O) * spost(Od) - spre(Od * O) - spost(Od * O)
+    return L
