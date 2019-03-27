Some updates to the calculation of dynamics. More parameters, more points, correct unit conversions.

TLS_dynamics.py
@@ -10,10 +10,11 @@ import exact_IB as exact
 import scipy as sp
 import phonon_weak_coupling as WC
 from utils import J_overdamped, beta_f, J_underdamped, J_minimal_hard
-reload(RC)
-reload(EM)
-reload(check)
-reload(exact)
+import imp
+imp.reload(RC)
+imp.reload(EM)
+imp.reload(check)
+imp.reload(exact)
 plt.style.use('ggplot')
 def steadystate_coupling_dependence(couplings, T_ph, eps, Gamma, w0, T_EM, Gamma_EM, overdamped=True):
     plt.figure()
@@ -30,7 +31,7 @@ def steadystate_coupling_dependence(couplings, T_ph, eps, Gamma, w0, T_EM, Gamma
     i = 1
     for alpha_ph in couplings:
         alpha_ph = alpha_ph*eps
-        print i
+        print(i)
         i+=1
         L_RC, H_RC, A_EM, A_nrwa, wRC, kappa, Gamma= RC.RC_function_UD(sigma, eps, T_ph, Gamma, w0, alpha_ph, N)
         L_wc = WC.L_phonon(alpha_ph, beta, Gamma, w0, overdamped=overdamped)
@@ -74,14 +75,14 @@ def steadystate_bias_dependence(biases, T_ph, alpha_ph, Gamma, w0, cutoff, T_EM,
     rc_s = []
     rc_naive = []
     i = 1
-    print w0, wc
+    print(w0, wc)
     for eps in biases:
         if overdamped:
             optical_cutoff = 20000.
             Gamma = w0**2/cutoff
         else:
             w0 = eps+0.5*np.pi*alpha_ph # underdamped SD parameter omega_0
-        print i
+        print(i)
         i+=1
         L_RC, H_RC, A_EM, A_nrwa, wRC, kappa, Gamma= RC.RC_function_UD(sigma, eps, T_ph, Gamma, w0, alpha_ph, N)
         L_wc = WC.L_phonon(alpha_ph, beta, Gamma, w0, overdamped=overdamped)
@@ -130,7 +131,7 @@ def steadystate_mode_dependence(mode_freqs, eps, T_ph, alpha_ph, Gamma, T_EM, Ga
     rc_naive = []
     i = 1
     for w0 in mode_freqs:
-        print i
+        print(i)
         i+=1
         L_RC, H_RC, A_EM, A_nrwa, wRC, kappa, Gamma= RC.RC_function_UD(sigma, eps, T_ph, Gamma, w0, alpha_ph, N)
         L_wc = WC.L_phonon(alpha_ph, beta, Gamma, w0, overdamped=False)
@@ -249,16 +250,16 @@ if __name__ == "__main__":
         # Calculate dynamics
         opts = qt.Options(nsteps=15000)
         if not phonon_only:
-            print "WEAK_COUPLING"
+            print("WEAK_COUPLING")
             #DATA_wc = mesolve(H_S, initial_sys, timelist, [L_wc+L_wc_EM], expects_wc, progress_bar=True)
-            print "NON-ROTATING-WAVE"
+            print("NON-ROTATING-WAVE")
             DATA_nrwa = mesolve(H_RC, rho_0, timelist, [L_RC+L_nrwa1], expects, progress_bar=True, options=opts)
             #DATA_nrwa2 = mesolve(H_RC, rho_0, timelist, [L_RC+L_nrwa2], expects, progress_bar=True, options=opts)
-            print "NON-SECULAR"
+            print("NON-SECULAR")
             #DATA_ns = mesolve(H_RC, rho_0, timelist, [L_RC+L_ns], expects, progress_bar=True)
-            print "SECULAR"
+            print("SECULAR")
             #DATA_s = mesolve(H_RC, rho_0, timelist, [L_RC+L_s], expects, progress_bar=True)
-            print "NAIVE"
+            print("NAIVE")
             #DATA_naive = mesolve(H_RC, rho_0, timelist, [L_RC+L_naive], expects, progress_bar=True)
             plt.figure()
             colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
@@ -303,7 +304,7 @@ if __name__ == "__main__":
             plt.legend()
 
             import frequency_analysis as fa
-            reload(fa)
+            imp.reload(fa)
             plt.figure()
             fa.plot_frequency(DATA_wc, timelist, label='WC', QOBJ=True)
             fa.plot_frequency(DATA_sc, timelist, label='SC', QOBJ=True)

UD_liouv.py
@@ -17,7 +17,8 @@ import numpy as np
 import scipy as sp
 from qutip import destroy, tensor, qeye, spre, spost, sprepost, Qobj
 import utils as UTILS
-reload(UTILS)
+import imp
+imp.reload(UTILS)
 
 #import pdb; pdb.set_trace()
 
@@ -45,7 +46,7 @@ def Ham_RC_gen(H_sub, sigma, Omega, kappa, N, rotating=False, shift = True, shif
     energy_shift = (kappa**2)/Omega
     I_sys = Qobj(qeye(H_sub.shape[0]),dims=sigma.dims)
     if shift and shift_op is not None:
-        print "Shifted"
+        print("Shifted")
         H_sub += shift_op*energy_shift
     if rotating:
         # Hopefully removes energy scale. Shift operator should be the same as
@@ -167,7 +168,7 @@ def RC_function_UD(sigma, eps, T_ph, Gamma, wRC, alpha_ph, N, silent=False,
     kappa= np.sqrt(np.pi * alpha_ph * wRC / 2.)  # coupling strength between the TLS and RC
 
     if not silent:
-        print "w_RC={} | TLS splitting = {} | RC-res. coupling={:0.2f} | TLS-RC coupling={:0.2f} | Gamma_RC={:0.2f} | alpha_ph={:0.2f} | N={} |".format(wRC, eps, gamma,  kappa, Gamma, alpha_ph, N)
+        print("w_RC={} | TLS splitting = {} | RC-res. coupling={:0.2f} | TLS-RC coupling={:0.2f} | Gamma_RC={:0.2f} | alpha_ph={:0.2f} | N={} |".format(wRC, eps, gamma,  kappa, Gamma, alpha_ph, N))
     H, A_em, A_nrwa, A_ph = Ham_RC(sigma, eps, wRC, kappa, N, rotating=rotating)
     if new:
         L_RC, Z =  liouvillian_build_new(H, A_ph, gamma, wRC, T_ph)
@@ -186,7 +187,7 @@ def RC_function_gen(H_sub, sigma, T_ph, Gamma, wRC, alpha_ph, N, silent=False,
     kappa= np.sqrt(np.pi * alpha_ph * wRC / 2.)  # coupling strength between the TLS and RC
 
     if not silent:
-        print "w_RC={} | RC-res. coupling={:0.2f} | TLS-RC coupling={:0.2f} | Gamma_RC={:0.2f} | alpha_ph={:0.2f} | N={} |".format(wRC, gamma,  kappa, Gamma, alpha_ph, N)
+        print("w_RC={} | RC-res. coupling={:0.2f} | TLS-RC coupling={:0.2f} | Gamma_RC={:0.2f} | alpha_ph={:0.2f} | N={} |".format(wRC, gamma,  kappa, Gamma, alpha_ph, N))
     H, A_em, A_nrwa, A_ph = Ham_RC_gen(H_sub, sigma, wRC, kappa, N,
                                         rotating=rotating,
                                         shift_op=shift_op, shift=shift)

driving_liouv.py
@@ -149,7 +149,7 @@ def integral_converge(f, a, omega):
         a+=inc
         x+=inc
         #time.sleep(0.1)
-    print "Integral converged"
+    print("Integral converged")
     return I # Converged integral
 
 def Gamma(omega, beta, J, alpha, wc, imag_part=True, c=1):
@@ -192,8 +192,8 @@ def L_non_rwa(H_vib, A, w_0, alpha, T_EM, J, principal=False,
     #J=J_minimal # J_minimal(omega, Gamma, omega_0)
     d_dim = len(eVals)
     G = 0
-    for i in xrange(d_dim):
-        for j in xrange(d_dim):
+    for i in range(d_dim):
+        for j in range(d_dim):
             eta = eVals[i]-eVals[j]
             s = eVecs[i]*(eVecs[j].dag())
             #print A.matrix_element(eVecs[i].dag(), eVecs[j])
@@ -206,7 +206,7 @@ def L_non_rwa(H_vib, A, w_0, alpha, T_EM, J, principal=False,
     L =  qt.spre(A*G) - qt.sprepost(G, A)
     L += qt.spost(G_dag*A) - qt.sprepost(A, G_dag)
     if not silent:
-        print "Calculating non-RWA Liouvilliian took {} seconds.".format(time.time()-ti)
+        print("Calculating non-RWA Liouvilliian took {} seconds.".format(time.time()-ti))
     return -L
 
 def RWA_system_ops(H_vib, S):
@@ -278,7 +278,7 @@ def L_nonsecular(H_vib, A, eps, Gamma, T, J, time_units='cm', silent=False):
     L = spre(A*X1) -sprepost(X1,A)+spost(X2*A)-sprepost(A,X2)
     L+= spre(A.dag()*X3)-sprepost(X3, A.dag())+spost(X4*A.dag())-sprepost(A.dag(), X4)
     if not silent:
-        print "It took ", time.time()-ti, " seconds to build the Non-secular RWA Liouvillian"
+        print("It took ", time.time()-ti, " seconds to build the Non-secular RWA Liouvillian")
     return -0.5*L
 
 def L_full_secular(H_vib, A, eps, Gamma, T, J, time_units='cm', silent=False):
@@ -327,8 +327,8 @@ def L_full_secular(H_vib, A, eps, Gamma, T, J, time_units='cm', silent=False):
                         if abs(r_down*coeff_2)>0:
                             L+= r_down*coeff_2*(spost(QP*LM)-sprepost(LM, QP))
     if not silent:
-        print "It took ", time.time()-ti, " seconds to build the secular Liouvillian"
-        print "Secular approximation kept {:0.2f}% of total ME terms. \n".format(100*float(terms)/(d*d*d*d))
+        print("It took ", time.time()-ti, " seconds to build the secular Liouvillian")
+        print("Secular approximation kept {:0.2f}% of total ME terms. \n".format(100*float(terms)/(d*d*d*d)))
     return -L
 
 def L_vib_lindblad(H_vib, A, eps, Gamma, T, J, time_units='cm', silent=False):
@@ -369,7 +369,7 @@ def L_vib_lindblad(H_vib, A, eps, Gamma, T, J, time_units='cm', silent=False):
                 L += lam_ij_sq*(0.5*(T1 + T2) - T3)
                 l+=1
     if not silent:
-        print "It took ", time.time()-ti, " seconds to build the vibronic Lindblad Liouvillian"
+        print("It took ", time.time()-ti, " seconds to build the vibronic Lindblad Liouvillian")
     return -L
 
 def L_EM_lindblad(splitting, col_em, Gamma, T, J, time_units='cm', silent=False):
@@ -380,5 +380,5 @@ def L_EM_lindblad(splitting, col_em, Gamma, T, J, time_units='cm', silent=False)
     L+= 2*np.pi*J(splitting, Gamma, splitting)*(EMnb+1)*(sprepost(col_em, col_em.dag())-0.5*(spre(col_em.dag()*col_em) +spost(col_em.dag()*col_em)))
     L+= 2*np.pi*J(splitting, Gamma, splitting)*EMnb*(sprepost(col_em.dag(), col_em)-0.5*(spre(col_em*col_em.dag())+ spost(col_em*col_em.dag())))
     if not silent:
-        print "It took ", time.time()-ti, " seconds to build the electronic-Lindblad Liouvillian"
+        print("It took ", time.time()-ti, " seconds to build the electronic-Lindblad Liouvillian")
     return L

exact_IB.py
@@ -37,11 +37,11 @@ def integrand_Im(omega, t, alpha, beta, Gamma, w0):
 
 
 def integral_converge(f, a):
-    step = 150.
+    step = 60.+np.random.random()
     x = step
     I = 0
     der = 1000.
-    while (abs(f(x))>5E-11):
+    while (abs(f(x))>1E-9):
         I += quad(f, a, x)[0]
         der = abs(f(a)-f(x))/step
         a+=step
@@ -84,17 +84,17 @@ def exact_solution_at_t(t, eps, alpha, beta, Gamma, w0, rho_init):
 def exact_dynamics(eps, alpha, wc, w0, Gamma, beta, rho_init, time_points, overdamped=False , silent=False):
     rho_t = []
     alpha = alpha
-    print "eps: {}, alpha: {}, w0: {}, Gamma: {}, beta: {}".format(eps, alpha, w0, Gamma, beta)
+    print("eps: {}, alpha: {}, w0: {}, Gamma: {}, beta: {}".format(eps, alpha, w0, Gamma, beta))
     if overdamped:
-        Gamma = w0**2/wc
+        Gamma = (w0**2)/wc
     ti = time.time()
     for t in time_points:
         rho_t.append(exact_solution_at_t(t, eps, alpha, beta, Gamma, w0, rho_init))
         if not silent:
             if len(rho_t)%40 == 0:
-                print "Exact solution {:0.3f} percent finished".format((float(len(rho_t))/len(time_points))*100)
+                print("Exact solution {:0.3f} percent finished".format((float(len(rho_t))/len(time_points))*100))
     if not silent:
-        print "Exact solution took {} to calculate.".format(time.time()- ti)
+        print("Exact solution took {} to calculate.".format(time.time()- ti))
     return rho_t
 
 def absorption_integrand(t, omega, eps, shift, alpha, beta, Gamma, omega_0):

fermionic_leads.py
@@ -226,7 +226,7 @@ def L_R_lead_dissipators(H, A, T_L=77., mu_L=1000.,
         L += -sprepost(Adag, Zm_1)+spost(Zm_1*Adag)
         L_leads.append(L)
     if not silent:
-        print "Calculating the lead dissipators took {} seconds.".format(time.time()-ti)
+        print("Calculating the lead dissipators took {} seconds.".format(time.time()-ti))
     return -L_leads[0],-L_leads[1]
 
 def nonadditive_current_voltage(eps1=500., eps2=900., U=0., T_L=77.,
@@ -315,7 +315,7 @@ def RC_function_UD(H, n_op, sigma_op, T_ph, Gamma, wRC, alpha_ph, N, silent=Fals
     kappa= np.sqrt(np.pi * alpha_ph * wRC / 2.)  # coupling strength between the TLS and RC
 
     if not silent:
-        print "w_RC={} | RC-res. coupling={:0.2f} | TLS-RC coupling={:0.2f} | Gamma_RC={:0.2f} | alpha_ph={:0.2f} | N={} |".format(wRC, gamma,  kappa, Gamma, alpha_ph, N)
+        print("w_RC={} | RC-res. coupling={:0.2f} | TLS-RC coupling={:0.2f} | Gamma_RC={:0.2f} | alpha_ph={:0.2f} | N={} |".format(wRC, gamma,  kappa, Gamma, alpha_ph, N))
     H, A_em, A_nrwa, A_ph = Ham_RC(H, n_op, sigma_op, wRC, kappa, N, rotating=rotating)
     L_RC, Z =  RC.liouvillian_build(H, A_ph, gamma, wRC, T_ph)
     return L_RC, H, A_em, A_nrwa, Z, wRC, kappa, Gamma
@@ -329,7 +329,7 @@ def current_vs_voltage_with_phonons(eps1=1000., eps2=1000., U= 0., T_ph=77,
     T_R = T_ph
     ti = time.time()
     mu_Ls = np. linspace(0, 30.*w0, 70)
-    print "RC would need {} states to fill electronic gap.".format(eps/w0)
+    print("RC would need {} states to fill electronic gap.".format(eps/w0))
     currents = []
 
     H = build_H(eps1, eps2, U)
@@ -346,8 +346,8 @@ def current_vs_voltage_with_phonons(eps1=1000., eps2=1000., U= 0., T_ph=77,
                          real_only=real_only)
         currents.append(current_from_L(H_RC, L_RC+L_L+L_R, L_R, E_RC))
         if (i%10)==0:
-            print 100*(float(i)/len(mu_Ls)), "% complete"
-    print "Took {} seconds.".format(time.time()-ti)
+            print(100*(float(i)/len(mu_Ls)), "% complete")
+    print("Took {} seconds.".format(time.time()-ti))
     return mu_Ls-mu_R, currents
 
 def current_vs_phonon_coupling(eps1, eps2, T_ph=77., Gamma=30., w0=70., U=0., N=10,
@@ -357,7 +357,7 @@ def current_vs_phonon_coupling(eps1, eps2, T_ph=77., Gamma=30., w0=70., U=0., N=
     ti = time.time()
     eps = abs(eps2-eps1)
     alpha_prop = np. linspace(0., 1., 15)
-    print "RC would need {} states to fill electronic gap.".format(eps/w0)
+    print("RC would need {} states to fill electronic gap.".format(eps/w0))
     currents_nonadd = []
     #timelist = np.linspace(0, 3/Gamma_EM, 360)
     H = build_H(eps1, eps2, U)
@@ -385,8 +385,8 @@ def current_vs_phonon_coupling(eps1, eps2, T_ph=77., Gamma=30., w0=70., U=0., N=
                                             L_Rfull, obs_ops, method=method))
         del L_Lfull, L_Rfull, L_RC, H_RC, A_EM, A_nrwa, Z
         if (i%10)==0:
-            print 100*(float(i)/len(alpha_prop)), "% complete"
-    print "Took {} seconds.".format(time.time()-ti)
+            print(100*(float(i)/len(alpha_prop)), "% complete")
+    print("Took {} seconds.".format(time.time()-ti))
     return alpha_prop,  np.array(currents_nonadd).T
 
 

spectra_functions.py
@@ -14,10 +14,11 @@ import scipy as sp
 import phonon_weak_coupling as WC
 from utils import J_overdamped, beta_f, J_underdamped, J_multipolar
 from utils import ground_and_excited_states, initialise_TLS
+import imp
 
-reload(RC)
-reload(EM)
-reload(exact)
+imp.reload(RC)
+imp.reload(EM)
+imp.reload(exact)
 plt.style.use('ggplot')
 plt.rcParams["axes.grid"] = True
 plt.rcParams["axes.edgecolor"] = "0.15"
@@ -64,7 +65,7 @@ def emission_spectra(init_sys, init_RC, prop_coupling, eps, Gamma, w0_prop=2.1,
 
     # electromagnetic bath liouvillians
     final_t = end_T_mult/Gamma_EM
-    print "final t: ", final_t
+    print("final t: ", final_t)
     timelist = np.linspace(0, final_t, int(T_increments*final_t))
     options = qt.Options(nsteps=nsteps, store_states=True, method=method, order=order)
     E_op = tensor(E*E.dag(), I_RC)
@@ -96,7 +97,7 @@ def emission_spectra(init_sys, init_RC, prop_coupling, eps, Gamma, w0_prop=2.1,
 
         tau_f *= tau_f_mult # extend tau
 
-        print "Completed initial dynamics calculations for {} in {}  seconds.".format(label[i], time.time()-ti)
+        print("Completed initial dynamics calculations for {} in {}  seconds.".format(label[i], time.time()-ti))
         ti = time.time()
         Lambda_0 = sigma_RC*sum(P)
         del P
@@ -114,18 +115,18 @@ def emission_spectra(init_sys, init_RC, prop_coupling, eps, Gamma, w0_prop=2.1,
         #freq, spec = qt.spectrum_correlation_fft(taulist, g_1) # old method
         spec-= min(spec)
         spec = spec/sum(spec)
-        freq, spec = zip(*sorted(zip(freq, np.array(spec).real)))
+        freq, spec = list(zip(*sorted(zip(freq, np.array(spec).real))))
 
         pop_list.append(pop)
         g1_data.append(g_1)
         spectrum_data.append(spec)
         freq_data.append(freq)
-        print "Completed correlation function calculations for {} in {} seconds.".format(label[i], time.time()-ti)
+        print("Completed correlation function calculations for {} in {} seconds.".format(label[i], time.time()-ti))
     return timelist, pop_list, taulist, g1_data, spectrum_data, freq_data
 
 
 
-reload(RC)
+imp.reload(RC)
 def absorption_spectra(prop_coupling, eps, Gamma, w0, T_ph=300., T_EM=0.,
                                     Gamma_EM=1., overdamped=True, N=3):
     # Start system+RC in adiabatic eigenstate and then time-evolve
@@ -183,7 +184,7 @@ def absorption_spectra(prop_coupling, eps, Gamma, w0, T_ph=300., T_EM=0.,
         ax1.plot(timelist, S1.real, label=label[i]+' real')
         ax2.plot(timelist, S1.imag, label=label[i]+' imag')
         #norm = np.array([(sigma_RC.dag()*sigma_RC*rho_t).tr() for rho_t in P])[0:tau_f*steps_per_tau]
-        print "Completed response function calculations for {} in {} seconds.".format(label[i], time.time()-ti)
+        print("Completed response function calculations for {} in {} seconds.".format(label[i], time.time()-ti))
 
         freq, spec = qt.spectrum_correlation_fft(timelist, S1)
         #spec-= min(spec)

utils.py
@@ -4,6 +4,7 @@ import scipy as sp
 from qutip import spre, spost, sprepost
 import qutip as qt
 import pickle
+import sympy
 
 
 ev_to_inv_cm = 8065.5
@@ -18,7 +19,9 @@ def save_obj(obj, name ):
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def Coth(x):
-    return (np.exp(2*x)+1)/(np.exp(2*x)-1)
+    #return (np.exp(2*x)+1)/(np.exp(2*x)-1)
+    return float(sympy.coth(x))
+    
 
 
 def Occupation(omega, T, time_units='cm'):
@@ -120,7 +123,7 @@ def initialise_TLS(init_sys, init_RC, states, w0, T_ph, H_RC=np.ones((2,2))):
         #coherence state
         if type(init_RC) == tuple:
             # take in a 2-tuple to initialise in coherence state.
-            print init_sys, init_RC
+            print(init_sys, init_RC)
             rho_left = states[concat_list[init_sys[0]][init_RC[0]]]
             rho_right = states[concat_list[init_sys[1]][init_RC[1]]].dag()
             init_rho = rho_left*rho_right